6fe383a0c39d14201f27694ad6b6191532ea22ee
shijiafeng <shijiafeng@tongmingmedia.com>
2023-05-30 01:35:31 +0800

    key1

--- src/example/midjourney.ts
+++ src/example/midjourney.ts
@@ -8,7 +8,7 @@ import fs from 'fs'
 import Midjourney from "src/midjourney";
 import { FormData } from "node-fetch";
 let options: Midjourney.Configuration = {
-    agent: new HttpsProxyAgent('http://127.0.0.1:7890'),
+    // agent: new HttpsProxyAgent('http://127.0.0.1:7890'),
     SERVER_ID: '1097741407516635277',
     SALAI_TOKEN: 'MTA3NTI3NjI4Njk2NjEwNDEzNA.GgKRt9.O0xEKhkytqfTKSyzFsg_Nw-S__5B2xWpXtpss8',
     CHANNEL_ID: '1100734545491398676'
@@ -20,39 +20,40 @@ async function main() {
 
 
 
-    // const TOKEN = 'MTEwNjA5NzM1OTI0MDMxODk3Nw.Gzy9Ia.a4RpzDeZwIjhfvnEPo8LnFUEVyZE_pDyz24jrQ'
-    // const CHANNEL_ID = options.CHANNEL_ID
-    // const rest = new REST({ version: '10', agent: new ProxyAgent('http://127.0.0.1:7890') }).setToken(TOKEN);
-    // try {
-    //     console.log(await rest.get(Routes.channelMessages(CHANNEL_ID)));
-    // } catch (error) {
-    //     console.error(error);
-    // }
+    const TOKEN = 'MTEwNjA5NzM1OTI0MDMxODk3Nw.Gzy9Ia.a4RpzDeZwIjhfvnEPo8LnFUEVyZE_pDyz24jrQ'
+    const CHANNEL_ID = options.CHANNEL_ID
 
-    // const client = new Client({
-    //     intents: [GatewayIntentBits.Guilds], rest: {
-    //         agent: new ProxyAgent('http://127.0.0.1:7890')
-    //     }
-    // });
+    const client = new Client({
+        intents: [GatewayIntentBits.Guilds], rest: {
 
-    // client.on('ready', () => {
-    //     console.log(`Logged in as ${client.user.tag}!`);
-    // });
+        }
+    });
 
-    // client.on('error', error => {
-    //     console.error('The WebSocket encountered an error:', error);
-    // });
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+    });
 
-    // client.login(TOKEN).catch(error => {
-    //     console.error(`An error occurred while logging in: ${error.message}`, error);
-    // });;
+    client.on('interactionCreate', async interaction => {
+        // if (!interaction.isChatInputCommand()) return;
+        // if (interaction.commandName === 'ping') {
+        //     await interaction.reply('Pong!');
+        // }
+        console.log(interaction, 'it')
+    });
+    client.on('error', error => {
+        console.error('The WebSocket encountered an error:', error);
+    });
+
+    client.login(TOKEN).catch(error => {
+        console.error(`An error occurred while logging in: ${error.message}`, error);
+    });;
 
     //     // client.login(TOKEN)
 
     // } catch (error) {
 
     // }
-    // return;
+    return;
     let attachmentsBody = await midjourney.Interactions.describe({
         filePath: __dirname + '/WechatIMG678.png',
     })
