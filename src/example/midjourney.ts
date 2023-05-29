75239736624646ad2873af8194b8ea1f190a5d1a
shijiafeng <shijiafeng@tongmingmedia.com>
2023-05-30 01:52:05 +0800

    key3

--- src/example/midjourney.ts
+++ src/example/midjourney.ts
@@ -1,12 +1,7 @@
 
-import { HttpsProxyAgent } from "https-proxy-agent";
-import chalk from 'chalk';
 import { Client, GatewayIntentBits } from 'discord.js';
-import { REST, Routes } from 'discord.js';
-import { ProxyAgent } from 'undici'
-import fs from 'fs'
 import Midjourney from "src/midjourney";
-import { FormData } from "node-fetch";
+import "dotenv/config";
 let options: Midjourney.Configuration = {
     // agent: new HttpsProxyAgent('http://127.0.0.1:7890'),
     SERVER_ID: '1097741407516635277',
@@ -16,13 +11,8 @@ let options: Midjourney.Configuration = {
 }
 let midjourney = new Midjourney(options)
 async function main() {
-
-
-
-
-    const TOKEN = 'MTEwNjA5NzM1OTI0MDMxODk3Nw.GQqYCf.1mj2SAC2mNvK6rU4u2ji2H1FAM4tGNrgJn58n0'
-    const CHANNEL_ID = options.CHANNEL_ID
-
+    const TOKEN = process.env.BOT_TOKEN
+    const CHANNEL_ID = options.CHANNEL_ID   
     const client = new Client({
         intents: [GatewayIntentBits.Guilds], rest: {
 
